1a7e275f457628a2d22a7fa2cd13ad9315049ded
Mohamed_Dorgham <dorgham.inbox@gmail.com>
2023-01-06 09:57:48 +0200

    lab6 v2

--- lab6/bookstore/bookstore/settings.py
+++ lab6/bookstore/bookstore/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'books',
-        'USER': 'Dorgham',
+        'USER': 'dorgham',
         'PASSWORD': '1322',
         'HOST':'127.0.0.1',
         'PORT':'3306',
